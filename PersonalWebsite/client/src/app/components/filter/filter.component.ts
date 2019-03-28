396eea318fbbf3cdd67a24b32faa0bc493aef534
jzhan160 <rogerzjc0113@gmail.com>
2019-03-28 14:39:21 -0400

    filter

--- PersonalWebsite/client/src/app/components/filter/filter.component.ts
+++ PersonalWebsite/client/src/app/components/filter/filter.component.ts
@@ -9,37 +9,36 @@ import { CookieService } from "ngx-cookie-service";
   templateUrl: "./filter.component.html",
   styleUrls: ["./filter.component.css"]
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit{
+  public templateId;
+
   constructor(
     private router: Router,
     private location: PlatformLocation,
     private userService: UserService,
     private cookieService: CookieService
    ) {}
-
   ngOnInit() {
-    var url;
+    let url;
     for (const i in this.location) {
-      if (i === "location") {
+      if (i === 'location') {
         url = this.location[i].href;
         break;
       }
     }
-    console.log(url);
     let arr = url.split("/");
     let domainName = arr[3];
-    console.log(domainName);
     this.userService.searchDomainName(domainName).subscribe(
       val => {
-        var templateId = val["templateId"];
         this.cookieService.set('domainDest', domainName);
-        const homeURL = "/home" + templateId;
-        this.router.navigate([homeURL]);
+        this.templateId = val['templateId'];
+        //const homeURL = '/home' + this.templateId;
+        //this.router.navigate([homeURL]);
       },
       error => {
          if (error.status === 404) {
-          console.log("No domain name");
-          this.router.navigate(["login"]);
+          console.log('No domain name');
+          this.router.navigate(['login']);
         }
       }
     );
